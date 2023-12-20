a981c63b6e4fea947aeda5ed32a72bcbaaaa928c
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2023-12-20 20:17:02 +0500

    latest

--- lib/app/modules/home/views/home_page.dart
+++ lib/app/modules/home/views/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home page')));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider.builder(
+              itemCount: 4,
+              itemBuilder: (context, index, pageViewIndex) {
+                return CachedNetworkImage(
+                  imageUrl:
+                      'https://image.tmdb.org/t/p/w780/iIvQnZyzgx9TkbrOgcXx0p7aLiq.jpg',
+                  fit: BoxFit.cover,
+                );
+              },
+              options: CarouselOptions(
+                viewportFraction: 1,
+                autoPlay: true,
+                autoPlayAnimationDuration: const Duration(milliseconds: 1500),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
